9969c8968bd720b9487cccb4c75f3c43ad41957d
Eesa Ayub <ay82.ee@gmail.com>
2025-06-16 19:29:47 +0100

    add release files

--- PyRunner/main.py
+++ PyRunner/main.py
@@ -8,7 +8,8 @@ import shutil
 console = Console()
 
 def main():
-    console.print("[bold blue]Welcome to your Python 🐍 terminal. Type `END` to finish[/bold blue]")
+    #====INPUT====
+    console.print("[bold blue]Welcome to your Python 🐍 and Bash terminal. Type `END` to finish[/bold blue]")
     code = []
     bash = []
     while True:
@@ -21,25 +22,41 @@ def main():
             continue
         code.append(user_input if not "END" in user_input else user_input.replace("END", "").strip())
         lines = ("\n".join(code))
+        bash_lines = ("\n".join(bash))
+        #====CODE EXECUTION====
         if user_input == "END":
+            #====PYTHON====
             try:
-                console.print(Panel(Text(lines, style="blue"), title="[bold blue]Code to run[/bold blue]"))
+                console.print(Panel(Text(lines, style="blue"), title="[bold blue]Python code to run[/bold blue]"))
                 run = console.input("[bold yellow]Run code? This will affect you system! (Y/n) [/bold yellow]").strip().lower()
                 if run.lower() in ["y", ""]:
                     try:
-                        with open("temp_script.bash", "w") as bash_file:
-                            for cmd in bash:
-                                bash_file.write(f"{cmd}\n")
-                        os.system("bash temp_script.bash")
-                        os.remove("temp_script.bash")
+
                         exec(lines, globals())
-                        exit()
                     except Exception as e:
                         console.print(Panel(Text(str(e), style="bold red")), justify="center")
-                        exit()
                 else:
-                    exit()
+                    continue
             except Exception as e:
                 console.print(Panel(Text(str(e), style="bold red")), justify="center")
-
+            #====BASH====
+            try:
+                if bash_lines:
+                    console.print(Panel(Text(bash_lines, style="blue"), title="[bold blue]Bash code to run[/bold blue]"))
+                    run_bash = console.input("[bold yellow]Run code? This will affect you system! (Y/n) [/bold yellow]").strip().lower()
+                    if run_bash.lower() in ["y", ""]:
+                        try:
+                            with open("temp_script.bash", "w") as bash_file:
+                                for cmd in bash:
+                                    bash_file.write(f"{cmd}\n")
+                            os.system("bash temp_script.bash")
+                            os.remove("temp_script.bash")
+                            exit()
+                        except Exception as e:
+                            console.print(Panel(Text(str(e), style="bold red")), justify="center")
+                            exit()
+                    else:
+                        exit()
+            except Exception as e:
+                console.print(Panel(Text(str(e), style="bold red")), justify="center")
 main()
